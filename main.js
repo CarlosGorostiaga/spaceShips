3ea5998acf276e13d4736e779426326913c55013
Carlos <carlosgorospo@gmail.com>
2025-06-17 21:10:01 +0200

    Control de movil

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-// main.js
-import MainMenuScene from './scenes/MainMenuScene.js';
-import GameScene from './scenes/GameScene.js';
-import GameOverScene from './scenes/GameOverScene.js';
-
 const config = {
   type: Phaser.AUTO,
   width: 480,
-  height: 1000, // nueva altura
+  height: 1000, // o el alto que estés usando
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
   backgroundColor: '#0d223a',
   physics: { default: 'arcade', arcade: { debug: false } },
   scene: [MainMenuScene, GameScene, GameOverScene]
 };
-
 const game = new Phaser.Game(config);
